94e5496102ec68a2c46a8678e9da3adbdff9621d
gaohanyu <2405662372@qq.com>
2022-07-10 17:02:55 +0800

    服务监控

--- src/main/java/com/ghy/boot/Springboot5Application.java
+++ src/main/java/com/ghy/boot/Springboot5Application.java
@@ -1,8 +1,9 @@
 package com.ghy.boot;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAdminServer
 @SpringBootApplication
 public class Springboot5Application {
 
